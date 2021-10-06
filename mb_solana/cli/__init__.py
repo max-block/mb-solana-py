4118cc9c0e17605a4099c347340370b587f44441
Max Block <maxblock@pm.me>
2021-10-06 17:32:11 +0300

    update

--- mb_solana/cli/__init__.py
+++ mb_solana/cli/__init__.py
@@ -1,11 +1,12 @@
 import click
 from click import Context
+from click_aliases import ClickAliasedGroup
 
 from mb_solana import __version__
 from mb_solana.cli.cmd import balance_cmd, example_cmd, generate_accounts_cmd, keypair_cmd, transfer_sol_cmd
 
 
-@click.group()
+@click.group(cls=ClickAliasedGroup)
 @click.option("-c", "--config/--no-config", "config_", default=False, help="Print config and exit")
 @click.option("-n", "--node", multiple=True, help="List of JSON RPC nodes, it overwrites node/nodes field in config")
 @click.version_option(__version__, help="Show the version and exit")
--- mb_solana/cli/cmd/generate_accounts_cmd.py
+++ mb_solana/cli/cmd/generate_accounts_cmd.py
@@ -1,16 +1,18 @@
 import click
-from solana.account import Account
 
+from mb_solana import solana_account
 from mb_solana.cli.helpers import print_json
 
 
 @click.command(name="generate-accounts", help="Generate new accounts")
 @click.option("--limit", "-l", type=int, default=5)
-def cli(limit: int):
+@click.option("--array", is_flag=True, help="Print private key in the array format.")
+def cli(limit: int, array: bool):
     result = {}
     for _ in range(limit):
-        acc = Account()
-        public_key = acc.public_key().to_base58().decode("utf-8")
-        private_key = acc.keypair().decode()
-        result[public_key] = private_key
+        acc = solana_account.generate_account()
+        private_key = acc.private_key_base58
+        if array:
+            private_key = solana_account.get_private_key_arr_str(acc.private_key_base58)
+        result[acc.public_key] = private_key
     print_json(result)
