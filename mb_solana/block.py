a4a0045dfafb25f66ffd4fa059ec04f57aa8b74e
Max Block <maxblock@pm.me>
2022-01-24 13:38:00 +0300

    update

---
+++ mb_solana/block.py
@@ -0,0 +1,54 @@
+from mb_std import Result
+from pydantic import BaseModel
+
+from mb_solana.solana_rpc import rpc_call
+
+
+class BlockTxCount(BaseModel):
+    slot: int
+    vote_tx_ok: int
+    vote_tx_error: int
+    non_vote_tx_ok: int
+    non_vote_tx_error: int
+
+
+def calc_block_tx_count(node: str, slot: int, timeout=10, proxy=None) -> Result[BlockTxCount]:
+    res = rpc_call(node=node, method="getBlock", params=[slot], timeout=timeout, proxy=proxy)
+    if res.is_error():
+        return res
+    vote_tx_ok = 0
+    vote_tx_error = 0
+    non_vote_tx_ok = 0
+    non_vote_tx_error = 0
+    vote_tx_keys = [
+        "SysvarS1otHashes111111111111111111111111111",
+        "SysvarC1ock11111111111111111111111111111111",
+        "Vote111111111111111111111111111111111111111",
+    ]
+    try:
+        txs = res.ok["transactions"]
+        for tx in txs:
+            is_error = tx["meta"]["err"] is not None
+            account_keys = tx["transaction"]["message"]["accountKeys"]
+            if len(account_keys) == 5 and vote_tx_keys == account_keys[2:]:
+                if is_error:
+                    vote_tx_error += 1
+                else:
+                    vote_tx_ok += 1
+            else:
+                if is_error:
+                    non_vote_tx_error += 1
+                else:
+                    non_vote_tx_ok += 1
+
+        return res.new_ok(
+            BlockTxCount(
+                slot=slot,
+                vote_tx_ok=vote_tx_ok,
+                vote_tx_error=vote_tx_error,
+                non_vote_tx_ok=non_vote_tx_ok,
+                non_vote_tx_error=non_vote_tx_error,
+            ),
+        )
+    except Exception as e:
+        return Result(error=f"exception: {str(e)}", data=res.dict())
